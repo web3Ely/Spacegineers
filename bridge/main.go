02951e19037801861dfb00014992f7994f11323f
web3Ely <eliyar.web3@gmail.com>
2023-12-29 11:40:09 -0800

    gRPC and Server v2
    setting up grpc and server for version 2 where I added a unary call and stream call

--- bridge/main.go
+++ bridge/main.go
@@ -1,1 +1,47 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	pb "spacegineers_context/protobufs"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const (
+	port = 8080
+)
+
+func main() {
+	conn, err := grpc.Dial(fmt.Sprintf("localhost:%d", port), grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Bridge could not connect to context: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewServicesClient(conn)
+
+	res, err := client.ContextService(context.Background(), &pb.DamageRequest{Damage: 1000})
+	if err != nil {
+		log.Fatalf("Could not send damage to context %v", err)
+	}
+	log.Printf("Response is: %s", res)
+
+	stream, err := client.StreamData(context.Background(), &pb.NullValue{})
+	if err != nil {
+		log.Fatalf("Error opening stream: %v", err)
+	}
+
+	for {
+		resp, err := stream.Recv()
+		if err != nil {
+			break
+		}
+
+		fmt.Printf("Received integer: %d\n", resp.GetResult())
+	}
+
+	fmt.Println("Stream closed.")
+}
--- context/main.go
+++ context/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"google.golang.org/grpc"
 	"log"
 	"net"
 	pb "spacegineers_context/protobufs"
+	"time"
+
+	"google.golang.org/grpc"
 )
 
 const (
@@ -16,14 +19,36 @@ type SpaceAndTime struct {
 	pb.UnimplementedServicesServer
 }
 
+func (s *SpaceAndTime) ContextService(ctx context.Context, req *pb.DamageRequest) (*pb.ResultResponse, error) {
+	log.Printf("Received a damage: %v", req.Damage)
+	return &pb.ResultResponse{Result: 555}, nil
+}
+
+func (s *SpaceAndTime) StreamData(req *pb.NullValue, stream pb.Services_StreamDataServer) error {
+	for i := 1; i <= 5; i++ {
+		resp := &pb.ResultResponse{Result: int32(i)}
+
+		if err := stream.Send(resp); err != nil {
+			return err
+		}
+
+		// Simulate some processing time (in a real scenario, perform meaningful work here)
+		time.Sleep(time.Second * 2)
+	}
+	return nil
+}
+
 func main() {
+	log.Println("Initializing the Context")
 	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", port))
 	if err != nil {
 		log.Fatalf("Failed to listen to the port %d: %v", port, err)
 	}
+	log.Printf("Listening to the localhost:%d", port)
 	grpcServer := grpc.NewServer()
 	pb.RegisterServicesServer(grpcServer, &SpaceAndTime{})
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("Failed to start the server: %v", err)
 	}
+	log.Println("Starting the gRPC Server")
 }
